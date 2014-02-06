696b17a3b7ebd42babf586443e179d24b81da643
oldhu <me@oldhu.com>
2014-02-06 22:53:46 +0800

    generate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gc2nike::Application.config.secret_key_base = '79207d9e9d18c4d76347a718a06d6e870d36dfdaee95ff5de6b8a1c4f95070092be4b133ddd8f2e24f17bced9ac8121c0db9e5caeb034eb9b2a528912be92b5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gc2nike::Application.config.session_store :cookie_store, key: '_gc2nike_session'
