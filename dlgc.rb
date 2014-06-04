4130e7433fe7bd2b297da4b528ae51c51da1fbf9
oldhu <me@oldhu.com>
2014-06-04 14:58:49 +0800

    download latest garmin connect tcx

---
+++ dlgc.rb
@@ -0,0 +1,41 @@
+require './connect'
+require './nike'
+require './activity'
+require './logging'
+
+include Logging
+
+def save_progress(id)
+  File.open("dlgc.last", "w") { |f| f.write(id) }
+end
+
+def read_progress
+  return nil unless File.exist?("dlgc.last")
+  File.read("dlgc.last")
+end
+
+def get_tcx_date(tcx)
+  data = Activity.new.parse_tcx_header(tcx)
+  return data[0][0,10]
+end
+
+def download_tcx(con, id)
+  tcx = con.get_tcx(id)
+  return if tcx.nil?
+  date = get_tcx_date(tcx)
+  File.open("tcx/#{date}-#{id}.tcx", "w") { |f| f.write(tcx) }    
+end
+
+activity_id = read_progress
+
+if activity_id.nil?
+  abort "need last activity id" unless ARGV.length > 0
+  activity_id = ARGV[0]
+end
+
+con = Connect.new
+
+con.each_activity_after_activity(activity_id) do |id|
+  download_tcx(con, id)
+  save_progress(id)
+end
